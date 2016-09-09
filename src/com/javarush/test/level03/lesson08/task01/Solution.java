9d3d15fe18e2f857a2a0cdceb6c2f0f3a4219551
vkiz <vk.fstor@gmail.com>
2016-09-09 13:03:17 +0400

    Add some tasks

---
+++ src/com/javarush/test/level03/lesson08/task01/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level03.lesson08.task01;
+
+/* Как захватить мир
+Ввести с клавиатуры число и имя, вывести на экран строку:
+«имя» захватит мир через «число» лет. Му-ха-ха!
+Пример: Вася захватит мир через 8 лет. Му-ха-ха!
+
+Последовательность вводимых данных имеет большое значение.
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String age = bufferedReader.readLine();
+        String name = bufferedReader.readLine();
+        System.out.println(name + " захватит мир через " + age + " лет. Му-ха-ха!");
+    }
+}
