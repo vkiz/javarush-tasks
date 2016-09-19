9dd16686008c189de97c6469b7146a0137c3b26d
vkiz <vk.fstor@gmail.com>
2016-09-19 23:48:47 +0400

    Add level 6 homework tasks

---
+++ src/com/javarush/test/level06/lesson11/bonus01/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level06.lesson11.bonus01;
+
+import java.io.*;
+
+/* Нужно исправить программу, чтобы компилировалась и работала
+Задача: Программа вводит два числа с клавиатуры и выводит их максимум в виде «Max is 25»
+*/
+
+public class Solution {
+    public static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader  = new BufferedReader(new InputStreamReader(System.in));
+        String max = "Max is ";
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        Solution.max = a > b ? a : b;
+        System.out.println(max + Solution.max);
+    }
+}
---
+++ src/com/javarush/test/level06/lesson11/bonus03/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.test.level06.lesson11.bonus03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* Задача по алгоритмам
+Задача: Написать программу, которая вводит с клавиатуры 5 чисел и выводит их в возрастающем порядке.
+Пример ввода:
+3
+2
+15
+6
+17
+Пример вывода:
+2
+3
+6
+15
+17
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader  = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[5];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        // пузырьковая сортировка (bubble sort) по возрастанию
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i: array) {
+            System.out.println(i);
+        }
+    }
+}
